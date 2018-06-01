ccf0fc423d4995b9bee41d113450afdf56afb829
Pedro Miguel Oliveira da Silva <pemiolsi@gmail.com>
2018-06-01 21:55:05 +0100

    Allow to check the state of the future

--- src/Future.ts
+++ src/Future.ts
@@ -1,14 +1,37 @@
+export enum FutureState {
+    Pending, Fulfilled, Rejected
+};
+
 export class Future<T> {
     readonly promise : Promise<T>;
 
-    protected internalResolve : ( value ?: T | PromiseLike<T> ) => void;
+    private _state : FutureState;
 
-    protected internalReject : ( reason ?: any ) => void;
+    public get isPending () : boolean { return this._state == FutureState.Pending }
+
+    public get isFulfilled () : boolean { return this._state == FutureState.Fulfilled }
+
+    public get isRejected () : boolean { return this._state == FutureState.Rejected }
+
+    public get isResolved () : boolean { return this.isFulfilled || this.isRejected }
 
     constructor () {
         this.promise = new Promise<T>( ( resolve, reject ) => {
-            this.resolve = resolve;
-            this.reject = reject;
+            this.resolve = ( value ) => {
+                if ( this.isPending ) {
+                    this._state = FutureState.Fulfilled;
+                    
+                    resolve( value );                    
+                }
+            };
+
+            this.reject = ( reason ) => {
+                if ( this.isPending ) {
+                    this._state = FutureState.Rejected;
+                    
+                    reject( reason );
+                }
+            };
         } );
     }
 
